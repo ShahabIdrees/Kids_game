a8adcf69f699b3d3f71b4241ebe556279ac24741
Shahab Idrees <shahabchattha@gmail.com>
2022-05-23 18:48:10 +0500

    Clicks Handled

--- app/src/main/java/com/example/kidsgame/Quiz.java
+++ app/src/main/java/com/example/kidsgame/Quiz.java
@@ -2,26 +2,56 @@ package com.example.kidsgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Quiz extends AppCompatActivity {
-
+    private String grass = "aceimnorsuvwxz";
+    private String sky = "bdfhklt";
+    private String root = "gjpqy";
+    private String Selected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_quiz);
         Button[] Options = new Button[3];
         Options[0] = findViewById(R.id.button2);
         Options[1] = findViewById(R.id.button3);
         Options[2] = findViewById(R.id.button4);
+        Button End = findViewById(R.id.button5);
         TextView Quest = findViewById(R.id.textView4);
         TextView Answer = findViewById(R.id.textView5);
+        //final String[] Selected = {null};
+        End.setOnClickListener(new View.OnClickListener (){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Quiz.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        for (Button option:Options) {
+                option.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
+                    Selected =  option.getText().toString();
+
+
+                }
+            });
+        }
+
+        for(int i = 0; i < 100; i++){
+
+        }
 
 
     }
+    
     protected char randomCharGenerator(){
         int randomNo = (int) ((Math.random() * 26)  + 1);
         char alphabet = (char)(96 + randomNo);
