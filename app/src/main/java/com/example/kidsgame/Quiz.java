4dfb5c976437350630911aee6a755e8df6d523ee
Shahab Idrees <shahabchattha@gmail.com>
2022-05-23 18:05:09 +0500

    Implemented Random Character Generator

--- app/src/main/java/com/example/kidsgame/Quiz.java
+++ app/src/main/java/com/example/kidsgame/Quiz.java
@@ -3,6 +3,8 @@ package com.example.kidsgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Quiz extends AppCompatActivity {
 
@@ -10,6 +12,20 @@ public class Quiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        Button[] Options = new Button[3];
+        Options[0] = findViewById(R.id.button2);
+        Options[1] = findViewById(R.id.button3);
+        Options[2] = findViewById(R.id.button4);
+        TextView Quest = findViewById(R.id.textView4);
+        TextView Answer = findViewById(R.id.textView5);
+
+
+
+    }
+    protected char randomCharGenerator(){
+        int randomNo = (int) ((Math.random() * 26)  + 1);
+        char alphabet = (char)(96 + randomNo);
+        return alphabet;
     }
 
 
