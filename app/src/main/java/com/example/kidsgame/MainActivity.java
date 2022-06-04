c265b4371e0bfcb79ea7dceab0768a7cf42a2cc2
Shahab Idrees <shahabchattha@gmail.com>
2022-06-04 23:45:59 +0500

    Clicks of the new features Handled

--- app/src/main/java/com/example/kidsgame/MainActivity.java
+++ app/src/main/java/com/example/kidsgame/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kidsgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,13 +14,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button startQuiz = findViewById(R.id.button);
+        Button learn = findViewById(R.id.button6);
+        Button github = findViewById(R.id.button7);
+        Button email = findViewById(R.id.button8);
+        startQuiz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Quiz.class);
                 startActivity(intent);
             }
         });
+        learn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Quiz.class);
+                startActivity(intent);
+            }
+        });
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("*/*");
+                intent.putExtra(Intent.EXTRA_EMAIL, "shahabchattha@gmail.com");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://github.com/ShahabIdrees");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+            }
+        });
+
     }
 }
